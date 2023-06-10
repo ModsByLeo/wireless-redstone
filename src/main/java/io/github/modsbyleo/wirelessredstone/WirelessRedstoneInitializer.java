90a91dbda0cbac4fabc127ef5d5f54202477bfef
ADudeCalledLeo <7997354+Leo40Git@users.noreply.github.com>
2023-06-10 13:57:19 +0300

    init!

---
+++ src/main/java/io/github/modsbyleo/wirelessredstone/WirelessRedstoneInitializer.java
@@ -0,0 +1,16 @@
+package io.github.modsbyleo.wirelessredstone;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+
+public class WirelessRedstoneInitializer implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("Wireless Redstone: Leo's Take");
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+		
+	}
+}
