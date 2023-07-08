2b2b05d8d4e60a4ad0428953d357d05805d5c7ae
ADudeCalledLeo <7997354+Leo40Git@users.noreply.github.com>
2023-07-08 18:15:04 +0300

    Revert "API draft v2, now with Components!"
    This reverts commit 7a0a6a6989c42d76d5cbf50e931f554b5c009d00.

--- src/main/java/io/github/modsbyleo/wirelessredstone/WirelessRedstoneInitializer.java
+++ src/main/java/io/github/modsbyleo/wirelessredstone/WirelessRedstoneInitializer.java
@@ -2,22 +2,13 @@ package io.github.modsbyleo.wirelessredstone;
 
 import com.mojang.serialization.JsonOps;
 import io.github.modsbyleo.wirelessredstone.api.RedstoneChannelKey;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import net.minecraft.util.Identifier;
 
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 
 public class WirelessRedstoneInitializer implements ModInitializer {
-	public static final String MOD_ID = "wireless_redstone_lt";
-
-	public static @NotNull Identifier id(@NotNull String path) {
-		return new Identifier(MOD_ID, path);
-	}
-
 	public static final Logger LOGGER = LoggerFactory.getLogger("Wireless Redstone: Leo's Take");
 
 	@Override
